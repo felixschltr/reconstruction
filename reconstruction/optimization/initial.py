8de1f95daa3790fc53c22c2f0667f8b7d8fb67e8
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-04-26 20:41:05 +0200

    fix bug in initial.py

--- reconstruction/optimization/initial.py
+++ reconstruction/optimization/initial.py
@@ -23,7 +23,7 @@ class OriginalImage(InitialGuessCreator):
 
     def __init__(self, image_class, image_id):
 
-        image = (ReconstructionImages & dict(image_class=image_class, image_id=image_id)).fetch1("image")
+        image = torch.from_numpy((ReconstructionImages & dict(image_class=image_class, image_id=image_id)).fetch1("image"))
         self.natural_img = image.repeat(1, 3, 1, 1)
 
     def __call__(self, *shape):
